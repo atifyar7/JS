af2a84f7cf03205f3e02a15affb5230c00c855c6
Muhammad Atif Yar <147138122+atifyar7@users.noreply.github.com>
2024-06-24 13:06:38 +0000

    container config done

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict"; //treat all js code as a newer version
+
+//alert(3*3) // we are using nodeJS not browser
+
+/*
+number
+string
+boolean
+null
+undefined
+symbol
+*/
---
+++ 01_basic/variables.js
@@ -0,0 +1,10 @@
+const accountId = 9362
+let accountEmail = "atifyar3@gmail.com"
+var accountPassword = "Pakistan123"
+accountCity = "Islamabad"
+let accountState;
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+console.log(typeof"pakistan123");
+console.log(typeof accountId);
