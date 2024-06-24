9f2175e1049608422e047c08c1664d664d38966e
Muhammad Atif Yar <147138122+atifyar7@users.noreply.github.com>
2024-06-24 14:20:31 +0000

    conversion of dataType

---
+++ 01_basic/03_conversionOperations.js
@@ -0,0 +1,18 @@
+let score= "7abc";
+
+//console.log(typeof score);
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+let isLoggedin= false
+//console.log(isLoggedin)
+
+let BoolisLoggedin = Number(isLoggedin)
+//console.log(BoolisLoggedin)
+
+let somenumber = 56
+let numToString= String(somenumber)
+console.log(numToString);
+console.log(typeof numToString)
