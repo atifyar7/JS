83ea3270f922d0ff58d07cb8549e1e11a25352b2
Muhammad Atif Yar <147138122+atifyar7@users.noreply.github.com>
2024-06-25 10:34:13 +0000

    Stack and Heap Memory

---
+++ 01_basic/05_Memory.js
@@ -0,0 +1,6 @@
+// ++++++++ Stack & Heap Memory ++++++++
+
+// Stack used in => premitive dataTypes
+// Heap used in => Non premitive dataTypes
+// premitive datatypes=> original value ki copy 
+// Non premitive dataTypes => oriinal value ka Reference
