96197e3ed1f53217bda89fd8f13794786f360fca
Muhammad Atif Yar <147138122+atifyar7@users.noreply.github.com>
2024-06-25 10:33:43 +0000

    Arrays, func, obj, operations

---
+++ 01_basic/04_operations.js
@@ -0,0 +1,26 @@
+let str1= "Muhammad"
+let str2= " Atif"
+let str3= " Yar"
+
+//console.log(str1+ str2+ str3);
+
+// console.log(1+2)
+
+// console.log(2 === 2)
+
+let names =["Atif", "Amir", "Faisal", "Usman", "Shuja"]
+
+// console.table(names)
+
+let myObj= {
+    name: "Atif",
+    age: 23,
+    city: "Islamabad",
+}
+// console.table(myObj)
+
+const myFun= function(){
+    console.log("Hello World")
+}
+
+console.log(myFun)
