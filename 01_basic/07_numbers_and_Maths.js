42ee2b902a761205917e1ac30f2f22723c70cb15
Muhammad Atif Yar <147138122+atifyar7@users.noreply.github.com>
2024-06-28 07:09:04 +0000

    Math & Num

---
+++ 01_basic/07_numbers_and_Maths.js
@@ -0,0 +1,27 @@
+// const score= 400;
+// console.log(score);
+
+const balance = new Number(250)
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+
+const anotherNumber= 1000000000
+// console.log(anotherNumber.toLocaleString());
+
+// +++++++++++++ Maths ++++++++++++++
+// console.log(Math)
+const score= new Number(36)
+// console.log(Math.abs(score))
+// console.log(Math.round(score));
+// console.log(Math.ceil(score));
+// console.log(Math.floor(score));
+// console.log(Math.sqrt(score));
+
+let RanValue = new Number;
+console.log(Math.floor(Math.random(RanValue)*10)+1)
+
+min= 20
+max= 30
+
+console.log(Math.floor(Math.random()*(max-min +1)+min))
