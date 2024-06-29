493b2e9f583d45d05f296a421b35dccef1908a55
Muhammad Atif Yar <147138122+atifyar7@users.noreply.github.com>
2024-06-29 06:12:25 +0000

    date and time

---
+++ 01_basic/08_date_and_time.js
@@ -0,0 +1,17 @@
+// let date = new Date()
+// console.log(date)
+// console.log(date.toDateString())
+// console.log(date.toLocaleDateString());
+// console.log(date.toISOString())
+
+// let myDate= new Date(2024, 5, 28)
+// console.log(myDate.toDateString())
+
+
+let myTimeSTamp= Date.now()
+console.log(Math.floor(Date.now()/1000))
+
+let anotherDate= new Date()
+console.log(anotherDate.getDay())
+console.log(anotherDate.getDate())
+console.log(anotherDate.getMonth()+1);
