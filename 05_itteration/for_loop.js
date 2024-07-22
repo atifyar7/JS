31d2e07d58587b973f5f5c794e184ea63cadeb64
Muhammad Atif Yar <147138122+atifyar7@users.noreply.github.com>
2024-07-22 07:43:27 +0000

    loops

--- 05_itteration/for_loop.js
+++ 05_itteration/for_loop.js
@@ -1,5 +1,13 @@
 for(let i=0; i<= 10; i++){
-    console.log(i);
+    // console.log(i);
 }
 
-console.log("Loop terminated");
+// console.log("Loop terminated");
+
+const myArray= ["Atif", "Amir", "Usman"]
+
+for (let index = 0; index < myArray.length; index++) {
+    // const element = myArray[index];
+    console.log(myArray[index]);
+    
+}
---
+++ 05_itteration/while_DoWhile_loop.js
@@ -0,0 +1,19 @@
+// let index= 0;
+// while(index<=10){
+//     console.log(`Value of index is ${index}`);
+//     index= index+2
+// }
+
+let myArray= ["Atif", "Amir", "Usman"]
+let index=0;
+while (index < myArray.length) {
+    // console.log(`Value is ${myArray[index]}`);
+    index++
+}
+
+let value= 0
+do{
+    console.log(`value is ${value}`)
+    value++
+} while(value<=10);
+
